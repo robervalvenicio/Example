934396ac59ce045e3d720d2db32a5b044f07ab5f
roberval venicio <rober_val_2@hotmail.com>
2023-06-25 16:53:30 -0300

    remoção de condicional para habilitar o swagger

--- Example/Program.cs
+++ Example/Program.cs
@@ -16,11 +16,10 @@ namespace Example
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
 
             app.UseHttpsRedirection();
 
